7c995f5eff4f27e65b1d365f49f4bc9a009a08e1
Mark Goldhardt <markgoldhardt@Marks-iMac.hsd1.ut.comcast.net>
2020-01-27 20:15:50 -0700

    fixed cards dynamic features

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// create component
+
+const articleCreator = (articleHeadline, articleAuthor, articleImg) => {
+
+    const newCard = document.createElement('div');
+    const newHeadline = document.createElement('div');
+    const newAuthor = document.createElement('div');
+    const newImg = document.createElement('div');
+    const newImgUrl = document.createElement('img');
+    const newAuthorName = document.createElement('span');
+
+    newCard.classList.add('card');
+    newHeadline.classList.add('headline');
+    newAuthor.classList.add('author');
+    newImg.classList.add('img-container');
+
+    newHeadline.textContent = articleHeadline;
+    newAuthorName.textContent = `By ${articleAuthor}`;
+    newImgUrl.src = articleImg;
+
+    newCard.appendChild(newHeadline);
+    newCard.appendChild(newAuthor);
+    newAuthor.appendChild(newImg);
+    newImg.appendChild(newImgUrl);
+    newAuthor.appendChild(newAuthorName);
+
+    return newCard;
+}
+
+// entry point
+
+const cardsContainer = document.querySelector('.cards-container');
+
+
+// get promise
+
+const articlesPromise = axios.get("https://lambda-times-backend.herokuapp.com/articles");
+
+articlesPromise.then(response => {
+    // console.log('res',response);
+    // console.log(response.data.articles);
+    
+    articlesArray = Object.values(response.data.articles);
+
+     articlesArray.forEach(item => {
+        item.forEach( article => cardsContainer.appendChild(articleCreator(article.headline, article.authorName, article.authorPhoto)))
+        //  console.log('item',item);
+     })
+})
